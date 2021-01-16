9111f9261de47c19867cb9744a17f99eaa060d2a
shahruj <57801700+shahruj@users.noreply.github.com>
2021-01-17 00:07:28 +0800

    Create record.py

---
+++ record.py
@@ -0,0 +1,48 @@
+import multiprocessing
+import tkinter as tk
+import cv2
+
+e = multiprocessing.Event()
+p = None
+
+# -------begin capturing and saving video
+def startrecording(e):
+    cap = cv2.VideoCapture(1)
+    out = out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (640,480))
+
+    while(cap.isOpened()):
+        if e.is_set():
+            cap.release()
+            out.release()
+            cv2.destroyAllWindows()
+            e.clear()
+        ret, frame = cap.read()
+        if ret==True:
+            out.write(frame)
+        else:
+            break
+
+def start_recording_proc():
+    global p
+    p = multiprocessing.Process(target=startrecording, args=(e,))
+    p.start()
+
+# -------end video capture and stop tk
+def stoprecording():
+    e.set()
+    p.join()
+
+    root.quit()
+    root.destroy()
+
+if __name__ == "__main__":
+    # -------configure window
+    root = tk.Tk()
+    root.geometry("%dx%d+0+0" % (100, 100))
+    startbutton=tk.Button(root,width=10,height=1,text='START',command=start_recording_proc)
+    stopbutton=tk.Button(root,width=10,height=1,text='STOP', command=stoprecording)
+    startbutton.pack()
+    stopbutton.pack()
+
+    # -------begin
+    root.mainloop()
